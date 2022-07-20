340fa46598a747c2da0a41edd54c2d1e4addb0c9
vishal6566 <vishaldas6566@gmail.com>
2022-07-21 01:09:21 +0530

    added

---
+++ script/storePage.js
@@ -0,0 +1,121 @@
+{
+    let count=0
+    
+    
+    function main1(){
+        let sub=document.getElementById("subhead1")
+       
+       
+       count++
+       
+       if(count%2==1){
+        sub.style.display="none"
+       }else{
+        sub.style.display="block"
+        sub.style.transition=" all 0.5s";
+        
+       }
+       
+    }
+}
+    
+
+{
+    let count=0
+    function main2(){
+        let sub=document.getElementById("subhead2")
+       
+       count++
+       if(count%2==0){
+        sub.style.display="none"
+       }else{
+        sub.style.display="block"
+       }
+        
+    }
+}
+{
+    let count=0
+    function main3(){
+        let sub=document.getElementById("subhead3")
+       
+       count++
+       if(count%2==0){
+        sub.style.display="none"
+       }else{
+        sub.style.display="block"
+       }
+        
+    }
+}
+{
+    let count=0
+    function main4(){
+        let sub=document.getElementById("subhead4")
+       
+       count++
+       if(count%2==0){
+        sub.style.display="none"
+       }else{
+        sub.style.display="block"
+       }
+        
+    }
+}
+{
+    let count=0
+    function main5(){
+        let sub=document.getElementById("subhead5")
+       
+       count++
+       if(count%2==0){
+        sub.style.display="none"
+       }else{
+        sub.style.display="block"
+       }
+        
+    }
+}
+{
+    let count=0
+    function main6(){
+        let sub=document.getElementById("subhead6")
+       
+       count++
+       if(count%2==0){
+        sub.style.display="none"
+       }else{
+        sub.style.display="block"
+       }
+        
+    }
+}
+{
+    let count=0
+    function main7(){
+        let sub=document.getElementById("subhead7")
+       
+       count++
+       if(count%2==0){
+        sub.style.display="none"
+       }else{
+        sub.style.display="block"
+       }
+        
+    }
+}
+{
+    let count=0
+    function main8(){
+        let sub=document.getElementById("subhead8")
+       
+       count++
+       if(count%2==0){
+        sub.style.display="none"
+       }else{
+        sub.style.display="block"
+       }
+        
+    }
+}
+
