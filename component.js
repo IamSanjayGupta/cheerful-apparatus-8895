0d97dd0870a5cfae6ef84b181c4a92c271f1be20
RohitSawant0351 <rohits1547@gmail.com>
2022-07-20 17:28:24 +0530

    Day-2

---
+++ component.js
@@ -0,0 +1,75 @@
+
+let subfooter= ()=>{
+    return `<h1 class="head"     >A complete suite of IAM and IT compliance solutions</h1>
+    <br>
+    <div id="rel-prod">
+        <div>Related Products</div>
+        </div>
+    <div id="related">
+        <div>
+            <a href=""><h4>» Active Directory Management & Reporting</h4></a>
+            <a href=""><h5>» Real-time Log Analysis and Reporting Solution</h5></a>
+            <a href=""><h5>» Microsoft 365 Management& Reporting Tool</h5></a>
+            <a href=""><h5>» Cloud Security & Log Management</h5></a>
+        </div>
+        <div>
+            <a href=""><h4>» Active Directory Auditing<h4></a>
+            <a href=""><h5>» Exchange Srever Auditing & Reporting</h5></a>
+            <a href=""><h5>» Active Directory Backup & Recovery tool</h5></a>
+            <a href=""><h5>» Comperhensive threat mitigation & SIEM (Log360)</h5></a>
+        </div>
+        <div>
+            <a href=""><h4>» Self-Service Password Management<h4></a>
+            <a href=""><h5>» File sever auditing & data discovery</h5></a>
+            <a href=""><h5>» SharePoint Management and Auditing Solution</h5></a>
+            <a href=""><h5>» Active Directory FREE Tools</h5></a>
+        </div>
+    </div>`
+}
+
+
+let topnavbar= ()=>{
+
+    return `  <div id="logo">
+    <img  src="https://www.manageengine.com/images/logo/manageengine-logo.svg" alt="">
+    <img  src="https://www.manageengine.com/images/me-20.png" alt="">
+</div>
+
+<div>
+    <h5>Products</h5>
+    <h5>Store</h5>
+    <h5>Store</h5>
+    <h5>Partners</h5>
+    <h5>Affliiate</h5>
+    <h5>Support</h5>
+    <p>|</p>
+    <a href=""><span class="material-symbols-outlined">
+        person
+        </span></a>
+        <span class="material-symbols-outlined">
+            search
+            </span>
+</div>
+`
+}
+
+
+let subnavbar= ()=>{
+
+    return ` <div>
+            <h1>AD360</h1>
+        </div>
+        <div>
+        <a href="">Overview</a>
+        <a href="./features.html">Features</a>
+        <a href="">Demo</a>
+        <a href="">Documents</a>
+        <a href="">Get Quote</a>
+        <a href="">Support</a>
+        <button>Download
+            </button>
+        </div>`
+}
+
+
+export  {subfooter,topnavbar,subnavbar};
