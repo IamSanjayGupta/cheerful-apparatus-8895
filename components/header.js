41172bf4a5e1aea1e7b137d3c143ee6ce626f2e4
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-07-21 10:00:57 +0530

    navbar Logo fixed

--- components/header.js
+++ components/header.js
@@ -12,8 +12,8 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function fixNavbar() {
    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
+      navbar.classList.add("bottomHeaderSticky");
    } else {
-      navbar.classList.remove("sticky");
+      navbar.classList.remove("bottomHeaderSticky");
    }
 }
