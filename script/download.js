4a745b569d06ade2d012efc8618e1666cf3f0f9c
RohitSawant0351 <rohits1547@gmail.com>
2022-07-23 20:08:40 +0530

    day-5

--- script/download.js
+++ script/download.js
@@ -4,3 +4,11 @@ document.getElementById("sub-footer").innerHTML= subfooter()
 document.getElementById("prod-navbar").innerHTML= topnavbar()
 document.getElementById("sub-navbar").innerHTML= subnavbar()
 
+
+let alertDownload= ()=>{
+
+    alert("Your file is Downloading, Please install after Completion")
+}
+
+document.getElementById("downloader").addEventListener("click", alertDownload)
+
